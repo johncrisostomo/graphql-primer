19aeeec9442193f6e44be8b835122bdd068d7ef5
John Dominic Crisostomo <john.dominic@aktify.com>
2017-08-05 01:02:29 +0800

    implements bidirectional relations

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,35 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 const axios = require('axios');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLSchema
+} = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      async resolve(parentValue, args) {
+        const { data } = await axios.get(
+          `https://localhost:3000/companies/${parentValue.id}/users`
+        );
+        return data;
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -27,7 +42,7 @@ const UserType = new GraphQLObjectType({
         return data;
       }
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
