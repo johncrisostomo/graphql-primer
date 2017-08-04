bfd207d2404c1c971dd8669100627cae5f77a9a4
John Dominic Crisostomo <john.dominic@aktify.com>
2017-08-04 22:36:06 +0800

    implements company resolver on the UserType

--- schema/schema.js
+++ schema/schema.js
@@ -3,12 +3,30 @@ const _ = require('lodash');
 const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      async resolve(parentValue, args) {
+        const { data } = await axios.get(
+          `http://localhost:3000/companies/${parentValue.companyId}`
+        );
+        return data;
+      }
+    }
   }
 });
 
