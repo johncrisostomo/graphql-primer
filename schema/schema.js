7a77e73f5c87d6754f5c5d597e504c03285226bb
John Dominic Crisostomo <john.dominic@aktify.com>
2017-08-04 22:06:39 +0800

    replaces users array with a dummy API endpoint from json-server

--- schema/schema.js
+++ schema/schema.js
@@ -1,11 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 }
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -22,8 +18,11 @@ const RootQuery = new GraphQLObjectType({
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
-      resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+      async resolve(parentValue, args) {
+        const { data } = await axios.get(
+          `http://localhost:3000/users/${args.id}`
+        );
+        return data;
       }
     }
   }
